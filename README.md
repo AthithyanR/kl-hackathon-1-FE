3d324f373d61a50a7bf4ceec87af3196db351b96
Sasikumar Chandrasekar <sasikumar.chandrasekar@kaaylabs.com>
2022-08-13 20:25:15 +0530

    Initial Setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # kl-hackathon-1-FE
+
+### Built With
+This project availabe Frameworks/Libraries
+
+* React
+* Mantine
+* Vite
+
+
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from '@mantine/core';
+import './App.scss';
+
+function App() {
+  return (
+    <Text className="text">Vite + React + Mantine</Text>
+  );
+}
+
+export default App;
