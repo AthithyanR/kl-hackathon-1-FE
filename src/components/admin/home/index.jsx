69bdf94faf411397af9175e0211f6b69723add2e
Sasikumar Chandrasekar <sasikumar.chandrasekar@kaaylabs.com>
2022-08-26 18:15:44 +0530

    Dashboard

--- src/components/admin/home/index.jsx
+++ src/components/admin/home/index.jsx
@@ -1,9 +1,125 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  createStyles, Navbar, Group,
+} from '@mantine/core';
+import {
+  IconBellRinging,
+  IconFingerprint,
+  IconKey,
+  IconSettings,
+  IconReceipt2,
+  IconSwitchHorizontal,
+  IconLogout,
+} from '@tabler/icons';
+import { MantineLogo } from '@mantine/ds';
 
-function AdminHome() {
+const useStyles = createStyles((theme, _params, getRef) => {
+  const icon = getRef('icon');
+  return {
+    header: {
+      paddingBottom: theme.spacing.md,
+      marginBottom: theme.spacing.md * 1.5,
+      borderBottom: `1px solid ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+      }`,
+    },
+
+    footer: {
+      paddingTop: theme.spacing.md,
+      marginTop: theme.spacing.md,
+      borderTop: `1px solid ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+      }`,
+    },
+
+    link: {
+      ...theme.fn.focusStyles(),
+      display: 'flex',
+      alignItems: 'center',
+      textDecoration: 'none',
+      fontSize: theme.fontSizes.sm,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      borderRadius: theme.radius.sm,
+      fontWeight: 500,
+
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+        [`& .${icon}`]: {
+          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        },
+      },
+    },
+
+    linkIcon: {
+      ref: icon,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+      marginRight: theme.spacing.sm,
+    },
+
+    linkActive: {
+      '&, &:hover': {
+        backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
+          .background,
+        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+        [`& .${icon}`]: {
+          color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+        },
+      },
+    },
+  };
+});
+
+const data = [
+  { link: '', label: 'Dashboard', icon: IconBellRinging },
+  { link: '', label: 'Quiz', icon: IconReceipt2 },
+  { link: '', label: 'Reports', icon: IconKey },
+  { link: '', label: 'User', icon: IconFingerprint },
+  { link: '', label: 'Settings', icon: IconSettings },
+];
+
+function NavbarSimple() {
+  const { classes, cx } = useStyles();
+  const [active, setActive] = useState('Billing');
+
+  const links = data.map((item) => (
+    <a
+      className={cx(classes.link, { [classes.linkActive]: item.label === active })}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ));
+
   return (
-    <div>AdminHome</div>
+    <Navbar height={700} width={{ sm: 300 }} p="md">
+      <Navbar.Section grow>
+        <Group className={classes.header} position="apart">
+          <MantineLogo size={28} />
+        </Group>
+        {links}
+      </Navbar.Section>
+      <Navbar.Section className={classes.footer}>
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+          <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
+          <span>Change account</span>
+        </a>
+
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+          <IconLogout className={classes.linkIcon} stroke={1.5} />
+          <span>Logout</span>
+        </a>
+      </Navbar.Section>
+    </Navbar>
   );
 }
 
-export default AdminHome;
+export default NavbarSimple;
---
+++ src/components/interview/quiz/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function InterviewHome() {
+  return (
+    <div>Quiz</div>
+  );
+}
+
+export default InterviewHome;
