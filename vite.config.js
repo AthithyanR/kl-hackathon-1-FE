19948e5d5076c5f25dcf7a94c1e22aef3dae688e
AthithyanR <athithyanr4@gmail.com>
2022-09-22 23:59:41 +0530

    c

--- vite.config.js
+++ vite.config.js
@@ -13,13 +13,13 @@ export default defineConfig({
       },
     },
   },
-  preview: {
-    proxy: {
-      '/api': {
-        target: 'http://194.195.116.226:8080',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // preview: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://194.195.116.226:8080',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 });
