b0aee5aecfbb4da4556148c5951fd20298ada7b0
suriyaraj13 <suriyaraj.baskaran@kaaylabs.com>
2022-09-22 18:05:57 +0530

    crud-operation

--- src/components/questions/index.jsx
+++ src/components/questions/index.jsx
@@ -1,15 +1,114 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable no-unused-vars */
-import { LoadingOverlay, Title } from '@mantine/core';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import React, { useState } from 'react';
+/* eslint-disable max-len */
+import React, { forwardRef, useState, useEffect } from 'react';
+import {
+  ActionIcon,
+  Grid,
+  Paper,
+  Select,
+  LoadingOverlay,
+  Title,
+  Box,
+  CloseButton,
+  Button,
+  Card,
+  Text,
+  Modal,
+  TextInput,
+  SegmentedControl,
+  Alert,
+} from '@mantine/core';
+import { IconPlaylistAdd, IconPencilPlus, IconTrash } from '@tabler/icons';
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query';
+import { useForm } from '@mantine/form';
+import { showNotification } from '@mantine/notifications';
+import { useNavigate } from 'react-router-dom';
+import AddQuestion from './upsert';
+import { queryConstants, questionTypes, dbMessageSnip } from '../../shared/constant-values';
 import baseApi from '../../shared/api';
-import { queryConstants, questionTypes } from '../../shared/constant-values';
-import { generateQS } from '../../shared/utils';
+import { generateQS, isEmpty } from '../../shared/utils';
+import './upsert/upsert.scss';
 
-function Questions() {
+const option = ({
+  label, image, onRemove, classNames, ...others
+}) => (
+  <div {...others}>
+    <Box
+      sx={(theme) => ({
+        display: 'flex',
+        cursor: 'default',
+        alignItems: 'center',
+        backgroundColor:
+          theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+        border: `1px solid ${
+          theme.colorScheme === 'dark'
+            ? theme.colors.dark[7]
+            : theme.colors.gray[4]
+        }`,
+        paddingLeft: 10,
+        borderRadius: 4,
+      })}
+    >
+      <Box mr={10}>
+        <img src={image} width="20" alt={label} />
+      </Box>
+      <Box sx={{ lineHeight: 1, fontSize: 12 }}>{label}</Box>
+      <CloseButton
+        onMouseDown={onRemove}
+        variant="transparent"
+        size={22}
+        iconSize={14}
+        tabIndex={-1}
+      />
+    </Box>
+  </div>
+);
+
+const Item = forwardRef(({ label, image, ...others }, ref) => (
+  <div ref={ref} {...others}>
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box mr={10} />
+      <img src={image} width="20" alt={label} />
+      <div>{label}</div>
+    </Box>
+  </div>
+));
+
+export default function Questions() {
+  const [opened, setOpened] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [selectOption, setSelectOption] = useState({ questionType: questionTypes[0] });
+  const [techTypeOf, setTechTypeOf] = useState(null);
+  const [modelKey, setModalKey] = useState('add');
+  const [editObject, setEditObject] = useState({});
+
+  const { questionType, techType } = selectOption;
   const queryClient = useQueryClient();
-  const [techType, setTechType] = useState(null);
-  const [questionType, setQuestionType] = useState(questionTypes[0]);
+  const navigate = useNavigate();
+
+  const fetchData = async () => {
+    try {
+      const responseData = await baseApi.get('/questions', {
+        params: {
+          techTypeId: techType,
+          questionType,
+        },
+      });
+      const { data: quiz } = responseData;
+      setQuestions(quiz);
+    } catch (err) {
+      console.log(`error while fetching data - ${err}`);
+    }
+  };
+
+  useEffect(() => {
+    if (selectOption.techType) {
+      fetchData();
+    }
+    console.log('useeffect', selectOption);
+  }, [selectOption]);
+  console.log('check', selectOption);
 
   const {
     data: techTypesResp,
@@ -18,24 +117,61 @@ function Questions() {
   } = useQuery(
     [queryConstants.techTypes],
     () => baseApi.get('/techTypes'),
+  );
+
+  const techTypes = techTypesResp?.data.map(({ id, imgUrl, name }) => ({
+    id,
+    value: id,
+    label: name,
+    image: imgUrl,
+  })) || [];
+
+  // setSelectOption(() => ({ ...selectOption, techType: techTypes[0]?.id }));
+
+  const setSingleValue = (key, value) => {
+    if (selectOption[key] === value) return;
+    setSelectOption({ ...selectOption, [key]: value });
+  };
+
+  const openModal = (key, visible, quiz) => {
+    setOpened(visible);
+    setModalKey(key);
+    setEditObject(quiz);
+  };
+
+  const deleteMutation = useMutation(
+    (payload) => baseApi.delete('/questions', { data: [payload] }),
     {
-      onSuccess: (data) => {
-        setTechType(data.data[0]?.name);
+      onSuccess: () => {
+        queryClient.invalidateQueries([queryConstants.questions]);
+      },
+      onError: (err) => {
+        showNotification({
+          message: err?.data?.includes(dbMessageSnip.foreignKey)
+            ? 'Please delete related questions!'
+            : 'Unable to delete question!',
+        });
       },
     },
   );
 
-  const {
-    data: questionsResp,
-    isLoading: isLoadingQuestions,
-    isError: isErrorQuestions,
-  } = useQuery(
-    [queryConstants.techTypes],
-    () => baseApi.get(`/questions${generateQS({ techType, questionType })}`),
-    {
-      enabled: !!(techTypesResp?.data && questionType),
-    },
-  );
+  const handleDelete = (id) => {
+    <Alert title="Alert!" color="red">
+      Are You Sure Want to delete this question
+      <div>
+        <div>
+          <Button onClick={navigate('/questions')}>
+            cancel
+          </Button>
+        </div>
+        <div>
+          <Button onClick={() => deleteMutation.mutate(id)}>
+            ok
+          </Button>
+        </div>
+      </div>
+    </Alert>;
+  };
 
   if (isLoadingTechTypes) {
     return <LoadingOverlay visible overlayBlur={2} />;
@@ -45,11 +181,122 @@ function Questions() {
     return <Title>Error occurred</Title>;
   }
 
-  const techTypes = techTypesResp.data || [];
-
   return (
-    <>{JSON.stringify(techTypes)}</>
+    <Paper p={10}>
+      <Grid justify="space-between">
+        <Grid.Col span={4}>
+          <Title order={2} mb={4}>
+            Questions
+          </Title>
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <ActionIcon
+            onClick={() => openModal('add', true)}
+            variant="filled"
+            sx={{ backgroundColor: '#211c57', marginLeft: 'auto' }}
+          >
+            <IconPlaylistAdd size={24} />
+          </ActionIcon>
+        </Grid.Col>
+      </Grid>
+      <Grid>
+        <AddQuestion
+          modelKey={modelKey}
+          opened={opened}
+          questionType={questionType}
+          setOpened={setOpened}
+          editObject={editObject}
+        />
+      </Grid>
+      <Grid justify="space-between">
+        <Grid.Col span={4}>
+          <Select
+            label="Tech Types"
+            data={techTypes}
+            // defaultValue={techTypes[1].id}
+            valueComponent={option}
+            itemComponent={Item}
+            searchable
+            size="md"
+            onChange={(e) => setSingleValue('techType', e)}
+            placeholder="Select Tech Type"
+          />
+        </Grid.Col>
+        <Grid.Col span={3}>
+          {questionTypes.map((type) => (
+            <Button
+              key={type}
+              className="option-btn"
+              style={{ backgroundColor: questionType === type ? '#6f6af8' : '#ffffff', color: '#343a40' }}
+              onClick={() => setSingleValue('questionType', type)}
+            >
+              {type}
+            </Button>
+          ))}
+        </Grid.Col>
+      </Grid>
+      <Grid>
+        <Grid.Col span={12}>
+          <Paper>
+            {!!questions.length && (
+              <Grid.Col
+                className="question"
+                mt={10}
+                span={12}
+              >
+                  {questions.map((q) => (
+                    <Card
+                      shadow="sm"
+                      mb={10}
+                      p={20}
+                      key={q.id}
+                    >
+                      {console.log('data-->', q)}
+                      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                        <Box>
+                          <ActionIcon
+                            mr={5}
+                            onClick={() => openModal('edit', true, q)}
+                            variant="light"
+                          >
+                            <IconPencilPlus />
+                          </ActionIcon>
+                        </Box>
+                        <Box>
+                          <ActionIcon
+                            mr={5}
+                            onClick={() => handleDelete(q.id)}
+                            variant="light"
+                          >
+                            <IconTrash />
+                          </ActionIcon>
+
+                        </Box>
+                      </div>
+                      <Text weight={500} size="md">
+                        {q.question}
+                        {' : '}
+                        {q.marks}
+                      </Text>
+                      <Card>
+                        {q.option1}
+                      </Card>
+                      <Card>
+                        {q.option2}
+                      </Card>
+                      <Card>
+                        {q.option3}
+                      </Card>
+                      <Card>
+                        {q.option4}
+                      </Card>
+                    </Card>
+                  ))}
+              </Grid.Col>
+            )}
+          </Paper>
+        </Grid.Col>
+      </Grid>
+    </Paper>
   );
 }
-
-export default Questions;
---
+++ src/components/questions/upsert/index.jsx
@@ -0,0 +1,199 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-use-before-define */
+/* eslint-disable max-len */
+import React, { useState, useEffect } from 'react';
+import {
+  Title, Grid, Modal, Textarea, LoadingOverlay, Group, Button, Card, Text, Checkbox, Input, NumberInput, TextInput,
+} from '@mantine/core';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+import { showNotification } from '@mantine/notifications';
+import { useForm } from '@mantine/form';
+import { queryConstants } from '../../../shared/constant-values';
+import baseApi from '../../../shared/api';
+import './upsert.scss';
+
+export default function AddQuestion(props) {
+  const {
+    opened, setOpened, modelKey, editObject,
+  } = props;
+  const [editId, setEditId] = useState(null);
+  const [addQuest, setAddQuest] = useState('');
+  let init = {
+    question: '',
+    option1: '',
+    option2: '',
+    option3: '',
+    option4: '',
+    correctoption: '',
+  };
+  console.log('modelkey', modelKey);
+  console.log('modelkehelncsy', editObject);
+  useEffect(() => {
+    if (modelKey === 'edit') {
+      init = {
+        questionType: editObject.questionType,
+        question: editObject.question,
+        option1: editObject.option1,
+        option2: editObject.option2,
+        option3: editObject.option3,
+        option4: editObject.option4,
+        correctOption: editObject.option,
+      };
+    }
+  });
+  const queryClient = useQueryClient();
+  const form = useForm({
+    initialValues: init,
+    validate: {
+      question: (Value) => (Value.length > 0 ? null : 'Please Enter Question'),
+      option1: (Value) => (Value.length > 0 ? null : 'Please Enter Options'),
+      option2: (Value) => (Value.length > 0 ? null : 'Please Enter Options'),
+      option3: (Value) => (Value.length > 0 ? null : 'Please Enter Options'),
+      correctoption: (Value) => (Value.length > 0 ? null : 'Please Enter Currect Option'),
+    },
+
+  });
+
+  const addMutation = useMutation(
+    (payload) => baseApi.post('/questions', [payload]),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries([queryConstants.questions]);
+        form.reset();
+        setOpened(false);
+      },
+      onError: () => {
+        showNotification({
+          title: '',
+          message: 'Unable to add new Question!',
+        });
+      },
+    },
+  );
+
+  const updateMutation = useMutation(
+    (payload) => baseApi.put('/questions', [payload]),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries([queryConstants.questions]);
+        form.reset();
+        setOpened(false);
+      },
+      onError: () => {
+        showNotification({
+          title: '',
+          message: 'Unable to update the tech type!',
+        });
+      },
+    },
+  );
+
+  const onCloseModal = () => {
+    setOpened(false);
+    form.reset();
+  };
+  return (
+    <Modal
+      title="Add Question"
+      opened={opened}
+      size="80%"
+      // fullScreen
+      onClose={() => onCloseModal()}
+      closeOnClickOutside={false}
+    >
+      <form
+        onSubmit={form.onSubmit(
+          editId ? updateMutation.mutate : addMutation.mutate,
+        )}
+      >
+        <Grid className="schedule-interview-model-content">
+          <Grid.Col
+            mt={10}
+          >
+            <Card>
+              <Textarea
+                placeholder="Add Question"
+                {...form.getInputProps('question')}
+                style={{}}
+              />
+              <div style={{ width: '100%' }}>
+                <div style={{
+                  display: 'flex', marginTop: '2%', justifyContent: 'space-between', width: '100%',
+                }}
+                >
+                  <div style={{ width: '48%' }}>
+                    <TextInput
+                      radius="sm"
+                      placeholder="Option 1"
+                      size="sm"
+                      {...form.getInputProps('option1')}
+                      withAsterisk
+                      addQuest={setAddQuest}
+                    />
+                  </div>
+                  <div style={{ width: '48%' }}>
+                    <TextInput
+                      placeholder="Option 2"
+                      radius="sm"
+                      size="sm"
+                      {...form.getInputProps('option2')}
+                      withAsterisk
+                    />
+                  </div>
+                </div>
+                <div style={{
+                  display: 'flex', justifyContent: 'space-between', width: '100%', marginTop: '2%',
+                }}
+                >
+                  <div style={{ width: '48%' }}>
+                    <TextInput
+                      placeholder="Option 3"
+                      radius="sm"
+                      size="sm"
+                      withAsterisk
+                      {...form.getInputProps('option3')}
+                    />
+                  </div>
+                  <div style={{ width: '48%' }}>
+                    <TextInput
+                      placeholder="Option 4"
+                      radius="sm"
+                      size="sm"
+                    />
+                  </div>
+                </div>
+                <div style={{
+                  display: 'flex', justifyContent: 'center', marginTop: '2%', width: '100%',
+                }}
+                >
+                  <div style={{ width: '80%' }}>
+                    <TextInput
+                      placeholder="Correct Option"
+                      radius="sm"
+                      size="sm"
+                      withAsterisk
+                      {...form.getInputProps('correctoption')}
+                    />
+                  </div>
+                </div>
+              </div>
+            </Card>
+            <Grid.Col
+              mt={10}
+              style={{ float: 'right' }}
+            >
+              <Button
+                mt="xl"
+                type="submit"
+                loading={addMutation.isLoading}
+              >
+                Add
+              </Button>
+            </Grid.Col>
+          </Grid.Col>
+
+        </Grid>
+      </form>
+    </Modal>
+  );
+}
