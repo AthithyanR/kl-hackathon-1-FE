e1738591a91ceaea02755cc1e0ff51ae6ba184d6
AthithyanR <athithyanr4@gmail.com>
2022-09-22 23:57:43 +0530

    c

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,18 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://194.195.116.226:8080',
-        // target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
     },
   },
   preview: {
-    port: 80,
+    proxy: {
+      '/api': {
+        target: 'http://194.195.116.226:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
